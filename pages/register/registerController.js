9ad3f6f6f5516c7a450d8959fd02b9fabcb6ea22
saarm <saarm24@gmail.com>
2019-06-13 13:59:43 +0300

    countries select categories select fit to new api

--- pages/register/registerController.js
+++ pages/register/registerController.js
@@ -6,39 +6,33 @@ angular.module("myApp")
 
         $http.get('http://localhost:3000/getCountries').then(function(response){
             console.log('entered??')
-            var temp=response.data;
+            var temp=response.data.countries;
             $scope.countries=temp;
             // $scope.countries=temp[0]["countryName"];
             console.log(temp[0]["countryName"]);
         });
 
 
-
         $http.get('http://localhost:3000/categories/getCategories').then(function(response){
             console.log('entered categories')
-            var categ=response.data.result;
-          //  var categ=respone.data;
+          //  var categ=response.data.result;
+           var categ=response.data.categories;
             $scope.categories=categ;
             // $scope.countries=temp[0]["countryName"];
            // console.log(categ[0]["result"]);
-
-            $scope.selection=[];
-
-
-
-            $scope.toggleSelection=function f(name){
-
-                var idx = $scope.selection.indexOf(name);
+            $scope.selectionCategory=[];
+            $scope.toggleSelectionCategory=function f(name){
+                var idx = $scope.selectionCategory.indexOf(name);
                 if (idx > -1) {
                     // is currently selected
-                    $scope.selection.splice(idx, 1);
+                    $scope.selectionCategory.splice(idx, 1);
                     console.log('ok deleted');
-                    console.log($scope.selection);
+                    console.log($scope.selectionCategory);
                 }
                 else {
-                    $scope.selection.push(name);
+                    $scope.selectionCategory.push(name);
                     console.log('ok inserted');
-                    console.log($scope.selection);
+                    console.log($scope.selectionCategory);
                 }
             }
 
