086308a5f1407b34ad569abeee86fcac25581dc2
avivc411 <avivc411@gmail.com>
2019-06-13 12:03:19 +0300

    set views add default view

--- app.js
+++ app.js
@@ -5,27 +5,37 @@ app.config(function($routeProvider)  {
     $routeProvider
         // homepage
         .when('/', {
-            // this is a template
-            template: '<h1>This is the default route</h1>'
-        })
-        // About
-        .when('/About', {
-            // this is a template url
-            templateUrl: 'pages/About/About.html',
-            controller : 'AboutController as abtCtrl'
-        })
-        // POI
-        .when('/POI', {
-            templateUrl: 'pages/POI/POI.html',
-            controller : 'POIController as poiCtrl'
-        })
-        .when('/httpRequest', {
-            templateUrl: 'pages/http/request.html',
-            controller : 'httpController as httpCtrl'
+            templateUrl: 'pages/DefaultView/DefaultView.html',
+            controller : 'DefaultViewController as DefaultViewCtrl'
         })
         .when('/register', {
             templateUrl: 'pages/register/draft.html',
             controller : 'registerController as regCtrl'
+        })
+        .when('/Login', {
+            templateUrl: 'pages/Login/Login.html',
+            controller : 'LoginController as LoginCtrl'
+        })
+        .when('/RetrievePassword', {
+            templateUrl: 'pages/RetrievePassword/RetrievePassword.html',
+            controller : 'RetrievePasswordController as RetrievePasswordCtrl'
+        })
+        .when('/About', {
+            // this is a template url
+            templateUrl: 'pages/About/About.html',
+            controller : 'AboutController as AboutCtrl'
+        })
+        .when('/POIS', {
+            templateUrl: 'pages/POIS/POIS.html',
+            controller : 'POISController as POISCtrl'
+        })
+        .when('/POI', {
+            templateUrl: 'pages/POI/POI.html',
+            controller : 'POIController as POICtrl'
+        })
+        .when('/Favorites', {
+            templateUrl: 'pages/Favorites/Favorites.html',
+            controller : 'FavoritesController as FavoritesCtrl'
         })
         // other
         .otherwise({ redirectTo: '/' });
