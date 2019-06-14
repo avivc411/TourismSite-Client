f3e1dfc23c8256083d9cacc304bbf995e3cfa36e
avivc411 <avivc411@gmail.com>
2019-06-14 18:07:00 +0300

    pois click change current point on rootScope

--- pages/POI/POIController.js
+++ pages/POI/POIController.js
@@ -1,8 +1,8 @@
 // POI controller
 angular.module("myApp")
     .controller("POIController", function ($window, $scope, $http, $rootScope) {
-        console.log($rootScope.point);
         self = this;
+        console.log($rootScope.point);
         $scope.point=$rootScope.point;
         $scope.ranks=[1,2,3,4,5];
         $scope.reviews=[];
@@ -11,37 +11,8 @@ angular.module("myApp")
             return $rootScope.point;
         }, function() {
             $scope.point = $rootScope.point;
-            /*ff();*/
         }, true);
 
-        /*$http.get('http://localhost:3000/points/getLastTwoReviews', {pointName:$rootScope.point.name})
-            .then(function successCallback(response) {
-                console.log("RESPONSE:", response.data);
-                $scope.reviews = response;
-                alert("Logged in!");
-            }, function errorCallback(response) {
-                alert("error " + response.data);
-            });*/
-
-        /*
-        function ff () {
-            console.log($scope.point.name)
-            var na=$scope.point.name;
-            alert(na);
-            var data = {
-                pointName: na
-            };
-            $http.get('http://localhost:3000/points/getLastTwoReviews', data)
-                .then(function successCallback(response) {
-                    console.log("RESPONSE:", response.data);
-                    $scope.reviews = response;
-                    alert("Logged in!");
-                }, function errorCallback(response) {
-                    alert("error " + response.data);
-
-                });
-        }
-        */
 
         // rank point
         $scope.rank=function (){
