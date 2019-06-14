79bc74fc6db16c2e2039642aef1c352ac4863b3f
avivc411 <avivc411@gmail.com>
2019-06-14 17:56:56 +0300

    pois sorting

--- pages/Favorites/FavoritesController.js
+++ pages/Favorites/FavoritesController.js
@@ -1,4 +1,20 @@
 // About controller
 angular.module("myApp")
 .controller("FavoritesController", function ($scope, $http) {
+    self = this;
+    $scope.points=[];
+    $http.get("http://localhost:3000/points/getAllPoints")
+        .then(function (response) {
+            $scope.points=$filter('orderBy')(response.data.points, 'rank');
+        }, function (response) {
+            alert(response.statusText)
+        });
+
+    $scope.sortRank = function(){
+        $scope.points=$filter('orderBy')($scope.points, 'rank');
+    };
+
+    $scope.sortCat = function(){
+        $scope.points=$filter('orderBy')($scope.points, 'category');
+    };
 });
--- pages/POIS/POISController.js
+++ pages/POIS/POISController.js
@@ -1,12 +1,42 @@
 // POI controller
 angular.module("myApp")
-    .controller("POISController", function ($scope, $http) {
+    .controller("POISController", function ($scope, $http, $filter) {
         self = this;
         $scope.points=[];
+        $scope.searchName='';
         $http.get("http://localhost:3000/points/getAllPoints")
             .then(function (response) {
-                $scope.points=response.data.points;
+                $scope.points=$filter('orderBy')(response.data.points, 'rank');
+                $scope.pointsBackup=$scope.points;
             }, function (response) {
                 alert(response.statusText)
-            })
+            });
+
+        $scope.sortRank = function(event){
+            $scope.points=$filter('orderBy')($scope.points, 'rank');
+            if ($scope.checked === event.target.value)
+                $scope.checked = false
+        };
+
+
+        $scope.sortCat = function(event){
+            $scope.points=$filter('orderBy')($scope.points, 'category');
+            if ($scope.checked === event.target.value)
+                $scope.checked = false
+        };
+
+        $scope.search = function () {
+            console.log($scope.pointsBackup);
+            let obj=$filter('filter')($scope.pointsBackup, {'name':$scope.searchName});
+            if(obj.length>0){
+                $scope.points=obj;
+            }
+            /**
+             * TODO:else show modal
+             */
+            else {
+                $scope.points=$scope.pointsBackup;
+                alert("No Points Found!");
+            }
+        }
     });
