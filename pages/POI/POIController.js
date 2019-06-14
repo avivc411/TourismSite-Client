8cb82c0627d2ac36b4b9ac999a51b221f42a7dfb
avivc411 <avivc411@gmail.com>
2019-06-14 18:38:42 +0300

    poi reviews

--- pages/POI/POIController.js
+++ pages/POI/POIController.js
@@ -2,31 +2,52 @@
 angular.module("myApp")
     .controller("POIController", function ($window, $scope, $http, $rootScope) {
         self = this;
-        console.log($rootScope.point);
         $scope.point=$rootScope.point;
         $scope.ranks=[1,2,3,4,5];
+        $scope.selectedRank=0;
         $scope.reviews=[];
+        console.log($rootScope.point);
+
+        $http({
+            method: 'GET',
+            url: 'http://localhost:3000/points/getLastTwoReviews/'+$rootScope.point.name,
+        }).then(function successCallback(response) {
+            $scope.reviews=response.data;
+        }, function errorCallback(response) {
+            console.log(response.data);
+        });
 
         $scope.$watch(function() {
             return $rootScope.point;
         }, function() {
             $scope.point = $rootScope.point;
+            $scope.getReviews();
         }, true);
 
+        $scope.getReviews=function(){
+            $http({
+                method: 'GET',
+                url: 'http://localhost:3000/points/getLastTwoReviews/'+$rootScope.point.name,
+            }).then(function successCallback(response) {
+                $scope.reviews=response.data.twoReviews;
+            }, function errorCallback() {
+                $scope.reviews=[];
+            });
+        };
 
         // rank point
         $scope.rank=function (){
-            //alert ($window.sessionStorage.getItem("token"));
             var req = {
                 method: 'POST',
                 url: 'http://localhost:3000/points/private/rankPoint',
                 headers: {
                     'x-auth-token':$window.sessionStorage.getItem("token")
                 },
-                data: { pointName: $scope.point.name, rank:$scope.selectedRank }
+                data: { pointName: $scope.point.name, rank: $scope.selectedRank }
             };
+
             $http(req).then(function(response) {
-                if (response.data.message==undefined)
+                if (response.data.message===undefined)
                     alert("Rank done ");
                 else
                     alert("Failed "+response.data.message);
