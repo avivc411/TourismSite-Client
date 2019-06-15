8c7bc52759ffa8de4acbe25d199974f7149cf0c6
saarm <saarm24@gmail.com>
2019-06-15 13:37:57 +0300

    after login directed popular points

--- pages/AfterLogin/AfterLoginController.js
+++ pages/AfterLogin/AfterLoginController.js
@@ -1,5 +1,39 @@
-// About controller
+// POI controller
 angular.module("myApp")
-    .controller("AfterLoginController", function ($scope, $http) {
+    .controller("AfterLoginController", function ($window, $scope, $http, $rootScope) {
+        self = this;
+       var u= $rootScope.user;
+        alert ("user is:"+u);
+
+
+
+            var req = {
+                method: 'GET',
+                url: 'http://localhost:3000/categories/private/getPopular',
+                headers: {
+                    'x-auth-token':$window.sessionStorage.getItem("token")
+                },
+                data: {}
+            };
+
+            $http(req).then(function(response) {
+                if (response.data.message===undefined) {
+                   $scope.FirstPopularpoint = response.data.twoPopularPointsInUsersCategories[0];
+                    $scope.SecondPopularpoint = response.data.twoPopularPointsInUsersCategories[1];
+                    console.log("temp is: "+$scope.FirstPopularpoint);
+                    alert("good ");
+                }
+                else
+                    alert("Failed "+response.data.message);
+                console.log(response.data);
+            }, function errorCallback(response) {
+                alert("error - "+response);
+                console.log(response);
+                console.log("error!")
+            });
+
+
+
+
 
     });
